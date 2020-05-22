3dc233970087d5cf7c47f8ae819d8faddd298135
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2020-05-22 17:01:42 -0400

    feat: finish macos integration

--- macos/RNCMaskedView.m
+++ macos/RNCMaskedView.m
@@ -8,17 +8,38 @@
 #import "RNCMaskedView.h"
 
 #import <React/NSView+React.h>
+#import <React/UIImageUtils.h>
 
 @implementation RNCMaskedView
+{
+  RCTView *_maskView;
+}
+
+- (instancetype)initWithFrame:(NSRect)frame
+{
+  if (self = [super initWithFrame:frame]) {
+    [self ensureLayerExists];
+    
+    _maskView = [[RCTView alloc] initWithFrame:NSZeroRect];
+    _maskView.canDrawSubviewsIntoLayer = YES;
+    [_maskView ensureLayerExists];
+  }
+  return self;
+}
+
+- (BOOL)isFlipped
+{
+  // HACK: Not sure why this is necessary when YES usually puts us in the top-left AFAIK.
+  // NOTE: This may have other side effects?
+  return NO;
+}
 
 - (void)didUpdateReactSubviews
 {
-  // RNCMaskedView expects that the first subview rendered is the mask.
-  NSView *maskView = [self.reactSubviews firstObject];
-  self.layer.mask = maskView.layer.mask;
-  // self.maskView = maskView;
+  // The first subview is always the mask.
+  [_maskView addSubview:self.reactSubviews[0]];
 
-  // Add the other subviews to the view hierarchy
+  // Add the other subviews to the view hierarchy.
   for (NSUInteger i = 1; i < self.reactSubviews.count; i++) {
     NSView *subview = [self.reactSubviews objectAtIndex:i];
     [self addSubview:subview];
@@ -27,9 +48,17 @@
 
 - (void)displayLayer:(CALayer *)layer
 {
-  // RNCView uses displayLayer to do border rendering.
-  // We don't need to do that in RNCMaskedView, so we
-  // stub this method and override the default implementation.
+  NSRect bounds = layer.bounds;
+  
+  // Ensure the maskView fits to our layer.
+  _maskView.frame = bounds;
+  
+  // The maskView is never drawn unless we do this.
+  NSBitmapImageRep *bitmap = [_maskView bitmapImageRepForCachingDisplayInRect:bounds];
+  [_maskView cacheDisplayInRect:bounds toBitmapImageRep:bitmap];
+  
+  // Use maskView.layer directly so animations are reflected in the mask.
+  layer.mask = _maskView.layer;
 }
 
 @end
